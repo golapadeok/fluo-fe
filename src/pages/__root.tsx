2e5b3a2ae746173ed3c39a4a8710dbb9ecb75d71
dkile <dkile532@naver.com>
2024-03-06 23:49:08 +0900

    env: tanstack router devtool 세팅

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,13 +1,26 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { lazy, Suspense } from "react";
 
 export const Route = createRootRoute({
 	component: Root,
 });
 
+const TanStackRouterDevtools =
+	process.env.NODE_ENV === "production"
+		? () => null
+		: lazy(() =>
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools,
+				})),
+		  );
+
 function Root() {
 	return (
-		<div>
+		<>
 			<Outlet />
-		</div>
+			<Suspense>
+				<TanStackRouterDevtools initialIsOpen={false} />
+			</Suspense>
+		</>
 	);
 }
