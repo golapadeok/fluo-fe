845b99b80e929103c158db285dbdc7df1c1e844f
dkile <dkile532@naver.com>
2024-03-12 02:57:42 +0900

    feat: msw 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import reactDom from "react-dom/client";
 import "./index.css";
 import App from "@/App";
 
-reactDom.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+async function enableMocking() {
+	if (process.env.NODE_ENV !== "development") {
+		return;
+	}
+
+	const { worker } = await import("@/mocks/browser");
+
+	return worker.start();
+}
+
+enableMocking().then(() =>
+	reactDom.createRoot(document.getElementById("root") as HTMLElement).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	),
 );
