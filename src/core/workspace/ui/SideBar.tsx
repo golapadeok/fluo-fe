56878355daae38ce5f0c142677ae779b3ac75956
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-03-17 11:53:15 +0900

    feat: workspace 목록 아코디언 구현

--- src/core/workspace/ui/SideBar.tsx
+++ src/core/workspace/ui/SideBar.tsx
@@ -24,8 +24,9 @@ function SideBar({ isOpen }: SideBarProps) {
 			style={{ left: `${distance}px` }}
 			className={`${
 				isOpen ? "hidden" : "flex"
-			} transition-all ease-out fixed z-10 w-[203px] inset-y-0 rounded-[20px] py-[14px] px-[11px] bg-bg-primary shadow-4`}
+			} flex-col gap-[14px] transition-all ease-out fixed z-10 w-[203px] inset-y-0 rounded-[20px] py-[14px] px-[11px] bg-bg-primary shadow-4`}
 		>
+			<span>검색하기</span>
 			<WorkspaceAccordion />
 		</aside>
 	);
--- src/core/workspace/ui/WorkspaceAccordion.tsx
+++ src/core/workspace/ui/WorkspaceAccordion.tsx
@@ -4,18 +4,49 @@ import {
 	AccordionItem,
 	AccordionTrigger,
 } from "@/lib/ui/accordion";
+import { ChevronDown, List } from "lucide-react";
+import { useState, type MouseEvent } from "react";
 
 function WorkspaceAccordion() {
+	const [isOpen, setIsOpen] = useState<boolean>(true);
+
 	return (
 		<Accordion
 			type="single"
-			defaultValue="item-1"
+			defaultValue="workspace-list"
 			collapsible
 			className="w-full"
+			onValueChange={(e: MouseEvent) => {
+				setIsOpen(!!e);
+			}}
 		>
-			<AccordionItem value="item-1">
-				<AccordionTrigger>테스트</AccordionTrigger>
-				<AccordionContent>내용물</AccordionContent>
+			<AccordionItem value="workspace-list">
+				<AccordionTrigger
+					className={`${
+						isOpen && "bg-bg-secondary"
+					} rounded-[5px] p-[7px] hover:no-underline`}
+				>
+					<div className="flex items-center gap-2 text-main-600 text-text-xs font-semibold">
+						<List />
+						워크스페이스 목록
+					</div>
+					<ChevronDown className="h-6 w-6 text-main-600 shrink-0 transition-transform duration-200" />
+				</AccordionTrigger>
+				<AccordionContent>
+					{/** @TODO map사용해야함. 클릭시 workspaceId로 이동하도록 추가해야함 */}
+					<div className="flex items-center p-1.5 text-zinc-400 text-text-xs">
+						워크스페이스 1
+					</div>
+					<div className="flex items-center p-1.5 text-zinc-400 text-text-xs">
+						워크스페이스 2
+					</div>
+					<div className="flex items-center p-1.5 text-zinc-400 text-text-xs">
+						워크스페이스 3
+					</div>
+					<div className="flex items-center p-1.5 text-zinc-400 text-text-xs">
+						워크스페이스 4
+					</div>
+				</AccordionContent>
 			</AccordionItem>
 		</Accordion>
 	);
