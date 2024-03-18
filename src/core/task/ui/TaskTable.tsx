5e03a83f4d30ec9fa95bbc2c1a28b434209c29b1
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-03-19 00:16:32 +0900

    refactor: 회의 때 이야기했던 TaskTable 수정 사항 반영

--- src/core/task/ui/TaskTable.tsx
+++ src/core/task/ui/TaskTable.tsx
@@ -18,10 +18,10 @@ const TaskTable = () => {
   )
 };
 
-const titleArray = ['프로젝트명', '프로젝트 요약/설명', '기한', '상태', '중요도', '담당자', '종류'];
+const titleArray = ['프로젝트명', '기한', '상태', '중요도', '담당자'];
 const TaskTableTitle = () => {
   return(
-    <div className=" grid grid-cols-7 w-full py-[18px] px-[47px] border-b">
+    <div className=" grid grid-cols-5 w-full py-[18px] px-[47px] border-b">
       {titleArray.map((title) => <div key={title} className="flex justify-center items-center grow">{title}</div>)}
     </div>
   )
@@ -29,16 +29,12 @@ const TaskTableTitle = () => {
 
 const TaskTableItem = () => {
   return(
-    <li className="py-[26px] px-[47px] grid grid-cols-7 w-full">
+    <li className="py-[26px] px-[47px] grid grid-cols-5 w-full">
       <div className="flex justify-center items-center">업무 제목</div>
-      <div className="flex justify-center items-center">업무에 대한 한줄 설명</div>
-      <div className="flex justify-center items-center">2024.04.08</div>
+      <div className="flex justify-center items-center">D-3</div>
       <div className="flex justify-center items-center">시작 전</div>
       <div className="flex justify-center items-center"><PriorityRating priority={4}/></div>
       <div className="flex justify-center items-center"><Avatar size="xs"/></div>
-      <div className="flex justify-center items-center">
-        <div className="w-[60px] h-5 py-0.5 px-3 rounded-sm bg-indigo-300 text-white text-xs">Design</div>
-      </div>
     </li>
   )
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import TaskTable from "@/core/task/ui/TaskTable";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -8,6 +9,7 @@ function Home() {
 	return(
 		<>
 			<div>홈페이지입니다.</div>
+			<TaskTable />
 		</>
 	);
 }
