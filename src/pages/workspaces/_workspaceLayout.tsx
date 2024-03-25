859ad0f5914a2ce388d6902ac8ffb6bf8e5a91ea
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-03-26 00:36:24 +0900

    design: 배경색 변경

--- src/pages/workspaces/_workspaceLayout.tsx
+++ src/pages/workspaces/_workspaceLayout.tsx
@@ -3,44 +3,38 @@ import { useState } from "react";
 import SubHeader from "@/core/workspace/ui/SubHeader";
 import SideBar from "@/core/workspace/ui/SideBar/SideBar";
 
-export const Route = createFileRoute(
-	"/workspaces/_workspaceLayout",
-)({
-	component: WorkSpaceLayoutComponent,
+export const Route = createFileRoute("/workspaces/_workspaceLayout")({
+  component: WorkSpaceLayoutComponent,
 });
 
 const routeApi = getRouteApi("/workspaces/_workspaceLayout/$workspaceId/");
 
 function WorkSpaceLayoutComponent() {
-	const routeParams = routeApi.useParams();
-	const { workspaceId } = routeParams;
-	const [isOpen, setIsOpen] = useState<boolean>(true);
+  const routeParams = routeApi.useParams();
+  const { workspaceId } = routeParams;
+  const [isOpen, setIsOpen] = useState<boolean>(true);
 
-	const handleToggleSidebar = () => {
-		setIsOpen(!isOpen);
-	};
-	return (
-		<>
-			<SideBar isOpen={!isOpen} />
-			<div className="bg-bg-secondary min-h-[100vh]">
-				<SubHeader
-					isOpen={isOpen}
-					onClickChevronButton={handleToggleSidebar}
-					workspaceData={{
-						name: "워크스페이스 이름",
-						id: workspaceId,
-					}}
-				/>
-				<main
-					className={`max-w-[1320px] m-auto ${
-						isOpen ? "pl-[236px]" : "pl-0"
-					} transition-all ease-linear`}
-				>
-					<Outlet />
-				</main>
-			</div>
-		</>
-	);
+  const handleToggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <SideBar isOpen={!isOpen} />
+      <div className="min-h-[100vh]">
+        <SubHeader
+          isOpen={isOpen}
+          onClickChevronButton={handleToggleSidebar}
+          workspaceData={{
+            name: "워크스페이스 이름",
+            id: workspaceId,
+          }}
+        />
+        <main className={`max-w-[1320px] m-auto ${isOpen ? "pl-[236px]" : "pl-0"} transition-all ease-linear`}>
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default WorkSpaceLayoutComponent;
