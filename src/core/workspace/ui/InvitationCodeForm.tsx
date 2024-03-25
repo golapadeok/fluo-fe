7307cce83dfcc9c2786d36bcca0d29585f35396e
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-03-25 14:41:07 +0900

    style: workspace 초대목록 디자인 수정

--- src/core/workspace/ui/InvitationCodeForm.tsx
+++ src/core/workspace/ui/InvitationCodeForm.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/lib/ui/button";
 
 function InvitationCodeForm() {
 	return (
-		<div className="flex flex-col gap-[14px] w-[417px] h-[172px] px-[23px] py-[26px] bg-bg-primary rounded-[10px]">
-			<h1 className="text-text-md font-bold">초대 코드 입력</h1>
-			<p className="text-text-md">관리자에게 전달받은 초대코드를 입력하세요</p>
+		<div className="flex flex-col gap-4 w-[448px] p-6 bg-bg-secondary rounded-[10px] shadow-1">
+			<h1 className="text-text-md font-bold">워크스페이스 초대하기</h1>
+
 			<form className="flex shrink-0 gap-3 w-full">
 				<input
 					className="w-full p-[10px] text-text-sm border-solid border-[1px] border-[#e2e2e2] bg-zinc-50 rounded-[10px] outline-none"
-					placeholder="초대코드를 입력해주세요."
+					placeholder="초대하고 싶은 멤버의 코드를 입력해주세요."
 				/>
 				<Button variant="primary" size="lg">
 					확인
