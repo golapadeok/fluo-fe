62f3e01d04d536b7397a9a94fb2aa9b70aa08a56
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-03-27 02:02:56 +0900

    design: 칸반 보드의 task 컴포넌트 수정

--- src/core/task/ui/StatusColumn.tsx
+++ src/core/task/ui/StatusColumn.tsx
@@ -1,13 +1,17 @@
 import Task from "@/core/task/ui/Task";
+import { Plus } from "lucide-react";
 
 const StatusColumn = () => {
   return (
     <div className="flex flex-col px-2.5 pt-3 gap-4 rounded-[10px] bg-zinc-100 min-w-[calc(25%-13.5px)] min-h-screen">
-      <div className="flex gap-3 items-center">
-        <div className="px-4 py-1 rounded-[17px] bg-[#D9D9D9]">To-do</div>
-        <div className="text-base">3</div>
+      <div className="flex gap-1 items-center">
+        <div className="px-4 py-1">To-do</div>
+        <div className="flex items-center justify-center text-sm font-semibold rounded-2xl text-white bg-main-600 px-2 py-0.5">3</div>
       </div>
       <div className="flex flex-col gap-4">
+        <button type="button" className="w-full px-28 py-2 rounded border border-zinc-300 bg-zinc-200">
+          <Plus className="text-[#A1A1AA] w-4 h-4" />
+        </button>
         <Task />
         <Task />
         <Task />
--- src/core/task/ui/Task.tsx
+++ src/core/task/ui/Task.tsx
@@ -4,14 +4,16 @@ import { CalendarClockIcon, User } from "lucide-react";
 
 const Task = () => {
   return (
-    <div className="flex flex-col justify-between px-4 py-[18px] rounded bg-[#E7E7E7] w-full h-32">
-      <div className="flex justify-between">
-        <div>
-          <div className="py-0.5 px-2 rounded-[3px] bg-zinc-400 text-xs text-white">#Design</div>
+    <div className="flex flex-col gap-[22px] px-4 py-[18px] rounded bg-white w-full">
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <div>
+            <div className="py-0.5 px-2 rounded-[3px] bg-zinc-400 text-xs text-white">#Design</div>
+          </div>
+          <PriorityRating priority={4} />
         </div>
-        <PriorityRating priority={4} />
+        <div className="text-xs font-semibold">업무 제목</div>
       </div>
-      <div className="text-xs font-semibold">업무 제목</div>
       <div className="flex justify-between">
         <div>
           <Avatar size="sm">
@@ -21,7 +23,7 @@ const Task = () => {
           </Avatar>
         </div>
         <div className="flex justify-center items-center gap-1">
-          <CalendarClockIcon className="w-4 h-4" />
+          <CalendarClockIcon className="w-4 h-4 text-zinc-500" />
           <p className=" text-[10px] text-zinc-500">D-7</p>
         </div>
       </div>
