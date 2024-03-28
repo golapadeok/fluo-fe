95ede8c4cecfe53f5e5a7f9f5d3fe8fa835f19de
dkile <dkile532@naver.com>
2024-03-28 22:37:00 +0900

    feat: msw 핸들러 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,63 @@
+import { mockWorkspaces, mockRoles, mockMembers } from "@/mocks/data";
 import { HttpResponse, http } from "msw";
 
 export const handlers = [
-	http.get("/", () => {
-		return HttpResponse.json({ intros: ["hi", "hello"] }, { status: 200 });
+	http.get("/workspaces/:workspaceId", ({ params }) => {
+		const { workspaceId } = params;
+
+		const workspace = mockWorkspaces.find(
+			(data) => data.workspaceId === workspaceId,
+		);
+
+		if (!workspace) return HttpResponse.error();
+
+		return HttpResponse.json(workspace, {
+			status: 200,
+		});
+	}),
+	http.put("/workspaces/:workspaceId", async ({ params, request }) => {
+		const { workspaceId } = params;
+		const title = await request.json();
+		const workspace = mockWorkspaces.find(
+			(data) => data.workspaceId === workspaceId,
+		);
+
+		if (!workspace || !title) return HttpResponse.error();
+
+		workspace.title = title.toString();
+
+		return HttpResponse.json(workspace, {
+			status: 201,
+		});
+	}),
+	http.delete("/workspaces/:workspaceId", ({ params }) => {
+		const { workspaceId } = params;
+
+		if (!workspaceId) return HttpResponse.error();
+
+		return HttpResponse.json(
+			{ message: "삭제했습니다" },
+			{
+				status: 200,
+			},
+		);
+	}),
+	http.get("/workspaces/:workspaceId/role", ({ params }) => {
+		const { workspaceId } = params;
+
+		if (!workspaceId) return HttpResponse.error();
+
+		return HttpResponse.json(mockRoles, {
+			status: 200,
+		});
+	}),
+	http.get("/workspaces/:workspaceId/members", ({ params }) => {
+		const { workspaceId } = params;
+
+		if (!workspaceId) return HttpResponse.error();
+
+		return HttpResponse.json(mockMembers, {
+			status: 200,
+		});
 	}),
 ];
